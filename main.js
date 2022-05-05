ba7e1294f395ba99ae0171de28c3250a961cbd8b
Douglas Figueiredo <devdouglasfigueiredo@gmail.com>
2022-05-05 17:37:26 -0300

    update

--- main.js
+++ main.js
@@ -20,5 +20,14 @@ function closeMenu (){
 ScrollReveal({
     origin: 'top',
     distance: '30px',
-    duration: 700
-}).reveal('#home');
+    duration: 700,
+}).reveal(`
+  #home,
+  #home img,
+  #home .stats,
+  #services,
+  #services header,
+  #services .card,
+  #about,
+  #about header,
+  #about .content`);
