473e0965d60035ffa9a0d69299c99439efe6f280
Douglas Figueiredo <devdouglasfigueiredo@gmail.com>
2022-05-06 17:34:25 -0300

    making responsive desktop

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
+window.addEventListener('scroll', onScroll);
+
 function onScroll (){
-    
+    showNavOnScroll();
+    showBackToTopButtonOnScroll()
+
+}
+
+function showNavOnScroll () {
     if (scrollY > 0){
         
         navigation.classList.add('scroll')
     } else {
         navigation.classList.remove('scroll')
     }
+}
 
+function showBackToTopButtonOnScroll (){
+    if (scrollY > 550){
+        
+        backToTopButton.classList.add('show')
+    } else {
+        backToTopButton.classList.remove('show')
+    }
 }
 
 function openMenu (){
@@ -31,3 +46,4 @@ ScrollReveal({
   #about,
   #about header,
   #about .content`);
+
